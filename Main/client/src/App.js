7ff86db101e4555e78316c5959c230e96791ce36
cmateo99 <126353236+cmateo99@users.noreply.github.com>
2023-06-02 04:57:58 -0700

    Update App.js

--- Main/client/src/App.js
+++ Main/client/src/App.js
@@ -19,7 +19,6 @@ import Navbar from './components/Navbar';
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
-
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
@@ -32,13 +31,11 @@ const authLink = setContext((_, { headers }) => {
     },
   };
 });
-
 const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-
 function App() {
   return (
 //Wrap everything in the provider
